5a642def5aee437d84154d708437c6f8c3491a82
Jérôme Vanderstichelen <jerome.vanderstichelen@soprabanking.com>
2022-02-11 18:04:20 +0100

    feat: build on pipeline

--- src/core/injector.rs
+++ src/core/injector.rs
@@ -3,17 +3,15 @@ use std::time::{Duration, Instant};
 pub struct Injector {
     pub(crate) url: String,
     pub(crate) duration: Option<Duration>,
-    pub(crate) response: Option<String>
 }
 
 impl Injector {
     pub(crate) fn inject(self) -> anyhow::Result<Injector> {
         let start = Instant::now();
-        let response = reqwest::blocking::get(&self.url)?.text()?;
+        reqwest::blocking::get(&self.url)?.text()?;
         let duration = start.elapsed();
         Ok(Self {
             duration: Some(duration),
-            response: Some(response),
             ..self
         })
     }
@@ -28,11 +26,10 @@ mod tests {
         let injector = Injector {
             url: "https://www.google.com".to_string(),
             duration: None,
-            response: None
         };
 
         let result = injector.inject();
-        assert_eq!(result.is_ok(),true);
+        assert_eq!(result.is_ok(), true);
     }
 
     #[test]
@@ -40,22 +37,9 @@ mod tests {
         let injector = Injector {
             url: "https://www.google.com".to_string(),
             duration: None,
-            response: None
         };
 
         let result = injector.inject().unwrap();
-        assert_eq!(result.duration.is_some(),true);
-    }
-
-    #[test]
-    fn inject_should_return_response() {
-        let injector = Injector {
-            url: "https://www.google.com".to_string(),
-            duration: None,
-            response: None
-        };
-
-        let result = injector.inject().unwrap();
-        assert_eq!(result.response.is_some(),true);
+        assert_eq!(result.duration.is_some(), true);
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,15 @@
-use crate::injector::injector::Injector;
+use crate::core::injector::Injector;
 
-mod injector;
+mod core;
 
 fn main() -> anyhow::Result<()> {
     let injector = Injector {
         url: "https://www.google.com".to_string(),
         duration: None,
-        response: None
     };
 
     let result = injector.inject().unwrap();
-    println!("{}",result.duration.unwrap().as_millis());
+    println!("{}", result.duration.unwrap().as_millis());
 
     Ok(())
 }
